1bd99982a12d94f95e2bc8a72c0e0fa1c3deb37a
SpiralMoon <mjh1660@gmail.com>
2021-05-17 14:02:23 +0900

    Update logic and bug fix

---
+++ android/src/main/java/dev/spiralmoon/simple_chromesafari/CustomTabsReceiver.java
@@ -0,0 +1,18 @@
+package dev.spiralmoon.simple_chromesafari;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class CustomTabsReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent i = new Intent(context, SimpleChromesafariPlugin.activity.getClass());
+
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        context.startActivity(i);
+    }
+}
--- android/src/main/java/dev/spiralmoon/simple_chromesafari/SimpleChromesafariPlugin.java
+++ android/src/main/java/dev/spiralmoon/simple_chromesafari/SimpleChromesafariPlugin.java
@@ -1,7 +1,11 @@
 package dev.spiralmoon.simple_chromesafari;
 
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.net.Uri;
+
+import androidx.browser.customtabs.CustomTabsIntent;
 
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -9,44 +13,62 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** SimpleChromesafariPlugin */
+/**
+ * SimpleChromesafariPlugin
+ */
 public class SimpleChromesafariPlugin implements MethodCallHandler {
-  /** Plugin registration. */
+    /**
+     * Plugin registration.
+     */
 
-  private final MethodChannel methodChannel;
-  public static Activity activity;
-  private Intent intent;
+    private final MethodChannel methodChannel;
+    public static Activity activity;
+    public static PendingIntent pendingIntent;
 
-  private SimpleChromesafariPlugin (Activity activity, MethodChannel methodChannel) {
-    System.out.println(activity.getLocalClassName());
-    this.activity = activity;
-    this.methodChannel = methodChannel;
-  }
-
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "simple_chromesafari");
-    channel.setMethodCallHandler(new SimpleChromesafariPlugin(registrar.activity(), channel));
-  }
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-
-    // open chrome custom tabs.
-    if (call.method.equals("openBrowser")) {
-
-      this.intent = new Intent(activity, BrowserActivity.class);
-      this.intent.putExtra("url", call.argument("url").toString());
-      //this.intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-
-      activity.startActivity(this.intent);
+    private SimpleChromesafariPlugin(Activity activity, MethodChannel methodChannel) {
+        System.out.println(activity.getLocalClassName());
+        SimpleChromesafariPlugin.activity = activity;
+        this.methodChannel = methodChannel;
     }
-    // close chrome custom tabs.
-    else if (call.method.equals("closeBrowser")) {
-      if (BrowserActivity.activity != null) {
-        BrowserActivity.activity.close();
-      }
-    } else {
-      result.notImplemented();
+
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "simple_chromesafari");
+        channel.setMethodCallHandler(new SimpleChromesafariPlugin(registrar.activity(), channel));
     }
-  }
+
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+
+        // open chrome custom tabs.
+        if (call.method.equals("openBrowser")) {
+            Uri uri = Uri.parse(call.argument("url").toString());
+            Intent intent = new Intent(activity, CustomTabsReceiver.class);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(activity, 0, intent, 0);
+
+            SimpleChromesafariPlugin.pendingIntent = pendingIntent;
+
+            CustomTabsIntent.Builder customTabsBuilder = new CustomTabsIntent.Builder();
+            CustomTabsIntent customTabsIntent = customTabsBuilder.build();
+            customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            customTabsIntent.launchUrl(activity, uri);
+
+            result.success(null);
+        }
+        // close chrome custom tabs.
+        else if (call.method.equals("closeBrowser")) {
+            if (pendingIntent != null) {
+                try {
+                    pendingIntent.send();
+                    pendingIntent = null;
+                    result.success(null);
+                } catch (Exception e) {
+                    result.error("Close failed.", e.getMessage(), e.getStackTrace());
+                }
+            } else {
+                result.success(null);
+            }
+        } else {
+            result.notImplemented();
+        }
+    }
 }
