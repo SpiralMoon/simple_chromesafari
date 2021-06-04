3d00d4a53405d74d0210b2ef536b8b8e166d34db
yoondeokn <yoondeokn@beflex.co>
2021-06-04 15:07:04 +0900

    Migration android v2

--- android/src/main/java/dev/spiralmoon/simple_chromesafari/CustomTabsReceiver.java
+++ android/src/main/java/dev/spiralmoon/simple_chromesafari/CustomTabsReceiver.java
@@ -1,5 +1,6 @@
 package dev.spiralmoon.simple_chromesafari;
 
+import android.app.Application;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -8,7 +9,7 @@ public class CustomTabsReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent i = new Intent(context, SimpleChromesafariPlugin.activity.getClass());
+        Intent i = new Intent(context, SimpleChromesafariPlugin.class);
 
         i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
--- android/src/main/java/dev/spiralmoon/simple_chromesafari/SimpleChromesafariPlugin.java
+++ android/src/main/java/dev/spiralmoon/simple_chromesafari/SimpleChromesafariPlugin.java
@@ -2,11 +2,14 @@ package dev.spiralmoon.simple_chromesafari;
 
 import android.app.Activity;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 
+import androidx.annotation.NonNull;
 import androidx.browser.customtabs.CustomTabsIntent;
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -16,25 +19,14 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 /**
  * SimpleChromesafariPlugin
  */
-public class SimpleChromesafariPlugin implements MethodCallHandler {
+public class SimpleChromesafariPlugin implements FlutterPlugin, MethodCallHandler {
     /**
      * Plugin registration.
      */
 
-    private final MethodChannel methodChannel;
-    public static Activity activity;
-    public static PendingIntent pendingIntent;
-
-    private SimpleChromesafariPlugin(Activity activity, MethodChannel methodChannel) {
-        System.out.println(activity.getLocalClassName());
-        SimpleChromesafariPlugin.activity = activity;
-        this.methodChannel = methodChannel;
-    }
-
-    public static void registerWith(Registrar registrar) {
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), "simple_chromesafari");
-        channel.setMethodCallHandler(new SimpleChromesafariPlugin(registrar.activity(), channel));
-    }
+    private MethodChannel methodChannel;
+    private PendingIntent pendingIntent;
+    private Context context;
 
     @Override
     public void onMethodCall(MethodCall call, Result result) {
@@ -42,15 +34,15 @@ public class SimpleChromesafariPlugin implements MethodCallHandler {
         // open chrome custom tabs.
         if (call.method.equals("openBrowser")) {
             Uri uri = Uri.parse(call.argument("url").toString());
-            Intent intent = new Intent(activity, CustomTabsReceiver.class);
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(activity, 0, intent, 0);
+            Intent intent = new Intent(context, CustomTabsReceiver.class);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
 
-            SimpleChromesafariPlugin.pendingIntent = pendingIntent;
+            this.pendingIntent = pendingIntent;
 
             CustomTabsIntent.Builder customTabsBuilder = new CustomTabsIntent.Builder();
             CustomTabsIntent customTabsIntent = customTabsBuilder.build();
             customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            customTabsIntent.launchUrl(activity, uri);
+            customTabsIntent.launchUrl(context, uri);
 
             result.success(null);
         }
@@ -70,5 +62,17 @@ public class SimpleChromesafariPlugin implements MethodCallHandler {
         } else {
             result.notImplemented();
         }
+    }
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        methodChannel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "simple_chromesafari");
+        methodChannel.setMethodCallHandler(this);
+        this.context = flutterPluginBinding.getApplicationContext();
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        methodChannel.setMethodCallHandler(null);
     }
 }
